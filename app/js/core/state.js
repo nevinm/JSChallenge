08e7feb6a6d61fee8120e14942ccfac773a220aa
Nevin Madhukar K <nevinm@qburst.com>
2016-02-06 11:34:58 +0530

    First commit after basic updates for UI

--- app/js/core/state.js
+++ app/js/core/state.js
@@ -1,27 +1,27 @@
 // Application Level State
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.when('', '/home');
+    $urlRouterProvider.when('', '/home');
 
 
-  $stateProvider
-    .state('app', {
-      url: '',
-      controller: 'AppCtrl',
-      views: {
-        'navbar': {
-          templateUrl: 'js/core/templates/navbar.tpl.html',
-          controller: 'NavbarCtrl'
-        },
-        'main': {
-          templateUrl: 'js/core/templates/main.tpl.html'
-        }
-      }
-    })
-    .state('404', {
-      url: '/404',
-      templateUrl: 'js/core/templates/404.tpl.html',
-      controller: 'AppCtrl'
-    });
+    $stateProvider
+        .state('app', {
+            url: '',
+            controller: 'AppCtrl',
+            views: {
+                'navbar': {
+                    templateUrl: 'js/core/templates/navbar.tpl.html',
+                    controller: 'NavbarCtrl'
+                },
+                'main': {
+                    templateUrl: 'js/core/templates/main.tpl.html'
+                }
+            }
+        })
+        .state('404', {
+            url: '/404',
+            templateUrl: 'js/core/templates/404.tpl.html',
+            controller: 'AppCtrl'
+        });
 
 }]);
